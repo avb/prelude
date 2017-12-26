648447eee3462a95c055fbc2d2d37eaf77f0def0
Adam Brown <avb@dicebag.com>
2017-12-26 19:08:38 +0000

    change C-k behavior and reorg neotree config

---
+++ personal/delete-line-no-kill.el
@@ -0,0 +1,9 @@
+;; Ctrl-K with no kill
+(defun delete-line-no-kill ()
+  (interactive)
+  (delete-region
+   (point)
+   (save-excursion (move-end-of-line 1) (point)))
+  (delete-char 1)
+  )
+(global-set-key (kbd "C-k") 'delete-line-no-kill)
