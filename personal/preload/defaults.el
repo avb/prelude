c26ae122822ea778e2097507bfae41cf2aabadd9
Adam Brown <adam.brown@fabricut.com>
2018-02-05 16:18:37 -0600

    update defaults and add sudo support

--- personal/preload/defaults.el
+++ personal/preload/defaults.el
@@ -1,1 +1,10 @@
 (setq backup-directory-alist `(("." . "~/.saves")))
+(setq-default tab-width 2)
+(setq-default truncate-lines t)
+(add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
+(add-hook 'web-mode-hook (lambda () (setq web-mode-markup-indent-offset 2)g))
+(add-hook 'web-mode-hook (lambda () (setq web-mode-css-indent-offset 2)))
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed 1) ;; don't accelerate scrolling
+(setq scroll-step 1) ;; keyboard scroll one line at a time
---
+++ personal/sudo.el
@@ -0,0 +1,5 @@
+(defadvice find-file (after find-file-sudo activate)
+  "Find file as root if necessary."
+  (unless (and buffer-file-name
+               (file-writable-p buffer-file-name))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
